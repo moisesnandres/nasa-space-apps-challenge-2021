8cfcc9776097df6c8f105d54cd2c4a4b26ce69ad
Moisés Ñañez <moisesnandres@hey.com>
2021-10-02 17:46:02 -0500

    Add initial map

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,15 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Track the plastic</title>
         <meta name="description" content="Leveraging AI/ML for plastic marine debris" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <h1>Home</h1>
-    </div>
+      <div className="map-container">
+        <div className="filters"></div>
+      </div>
+    </>
   )
 }
