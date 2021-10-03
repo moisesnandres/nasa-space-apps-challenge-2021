857d31f7bf46c4bdb0912655b977bcced8bbf2e4
Moisés Ñañez <moisesnandres@hey.com>
2021-10-03 09:00:29 -0500

    Ad first card for statistics

--- pages/statistics.js
+++ pages/statistics.js
@@ -1,15 +1,27 @@
 import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Statistics() {
   return (
-    <div>
+    <div className="statistic-container">
       <Head>
         <title>Track the plastic</title>
         <meta name="description" content="Leveraging AI/ML for plastic marine debris" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Statistics</h1>
+      <div className="wrapper">
+        <div className="left-column">
+          <div className="card info-card">
+            <div className="info-container">
+              <h1>Welcome to Track the plastic!</h1>
+              <p>Our mission is to share information about marine plastic debris pollution in our ocean, in order to take action and save our planet.</p>
+            </div>
+            <Image src="/images/fish_logo.png" alt="Fish Logo" width={143} height={137} />
+          </div>
+        </div>
+        <div className="right-column"></div>
+      </div>
     </div>
   )
 }
