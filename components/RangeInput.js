591841dcce89d5c3b15966ace4cb56eb901110f4
Moisés Ñañez <moisesnandres@hey.com>
2021-10-03 11:53:41 -0500

    Change input bar

--- components/RangeInput.js
+++ components/RangeInput.js
@@ -1,38 +1,9 @@
-import Box from '@mui/material/Box';
-import Slider from '@mui/material/Slider';
+import Image from 'next/image'
 
 export default function VerticalSlider() {
-  function preventHorizontalKeyboardNavigation(event) {
-    if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
-      event.preventDefault();
-    }
-  }
-
   return (
     <div className="range-container wrapper">
-      <Box sx={{ height: 300 }}>
-        <Slider
-          sx={{
-            '& input[type="range"]': {
-              WebkitAppearance: 'slider-vertical',
-            },
-          }}
-          orientation="vertical"
-          defaultValue={30}
-          aria-label="Temperature"
-          onKeyDown={preventHorizontalKeyboardNavigation}
-          marks={[
-            {
-              value: 0,
-              label: 'Low',
-            },
-            {
-              value: 100,
-              label: 'High',
-            },
-          ]}
-        />
-      </Box>
+      <Image src="/images/color_bar.png" alt="Bar" width={42} height={400} />
       <div className="range-name">Plastic Debris Concentration</div>
     </div>
   );
--- components/Zoom.js
+++ components/Zoom.js
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 
 export default function Zoom() {
-  const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0)
+  const handleCounter = (newCounter) => {
+    setCounter(newCounter)
+    // use another image or scale zoom
+  }
 
   return (
     <div className="zoom-magnifier">
-      <button disabled={counter === 0} onClick={() => setCounter(counter - 1)} className="minus">
+      <button disabled={counter === 0} onClick={() => handleCounter(counter - 1)} className="minus">
         -
       </button>
-      <button disabled={counter === 4} onClick={() => setCounter(counter + 1)} className="plus">
+      <button disabled={counter === 4} onClick={() => handleCounter(counter + 1)} className="plus">
         +
       </button>
     </div>
