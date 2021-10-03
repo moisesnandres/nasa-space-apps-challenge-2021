81d8bd70af11f53577c579744e0d9e0d7aaa9e81
Moisés Ñañez <moisesnandres@hey.com>
2021-10-03 11:12:52 -0500

    Add two more cards

--- pages/statistics.js
+++ pages/statistics.js
@@ -1,5 +1,8 @@
+import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Line } from 'react-chartjs-2';
+import { plasticWaste, plasticGrowth } from '../utils/fakeData'
 
 export default function Statistics() {
   return (
@@ -19,8 +22,49 @@ export default function Statistics() {
             </div>
             <Image src="/images/fish_logo.png" alt="Fish Logo" width={143} height={137} />
           </div>
+          <div className="card">
+            <h2>Countries that are polluting the oceans the most</h2>
+            <p>This is the annual metric of mismanaged plastic waste and the total ending up in global waters.</p>
+            <div className="charts">
+              <div className="plastic-waste">
+                <h4><div className="plastic-waste-info"></div>Mismanaged plastic waste</h4>
+                <ul>
+                  { plasticWaste.map((row, index) => (
+                    <li key={index}>
+                      <p className="country">{row.country}</p>
+                      <div className="bar">
+                        <div style={{ width: `${row.percentage * 2}px`}}></div>
+                        <p>{row.label} millions</p>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <h2>Plastic Debris Growth worldwide in the last 8 years</h2>
+            <Line
+              data={plasticGrowth}
+              width={200}
+              height={200}
+            />
+          </div>
         </div>
-        <div className="right-column"></div>
+        <div className="right-column">
+          <div className="card plastic-worldwide">
+            <h2>Did you knew that 80% of the trash that you find in the ocean is plastic?</h2>
+            <Image src="/images/continents.png" alt="Continents" width={670} height={344} />
+            <p>Do you want to see the real time tracking?</p>
+            <Link href="/">
+              <a className="cta">See tracking</a>
+            </Link>
+          </div>
+
+          <div className="card">
+            <h2>Plastic Debris by Ocean</h2>
+          </div>
+        </div>
       </div>
     </div>
   )
---
+++ utils/fakeData.js
@@ -0,0 +1,51 @@
+export const plasticWaste = [
+  {
+    country: 'China',
+    percentage: 88,
+    label: '8.80'
+  },
+  {
+    country: 'Indonesia',
+    percentage: 32,
+    label: '3.20'
+  },
+  {
+    country: 'Philippines',
+    percentage: 19,
+    label: '1.90'
+  },
+  {
+    country: 'Vietnam',
+    percentage: 18,
+    label: '1.80'
+  },
+  {
+    country: 'Sri Lanka',
+    percentage: 16,
+    label: '1.60'
+  }
+]
+
+export const plasticGrowth = {
+  labels: ['2014', '2015', '2016', '2017', '2018', '2019', '2020'],
+  datasets: [
+    {
+      label: "Growth",
+      fill: false,
+      lineTension: 0.1,
+      borderColor: '#027EC6',
+      borderCapStyle: 'butt',
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: 'miter',
+      pointBorderColor: '#C32753',
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [40, 46, 55, 59, 75, 78, 81]
+    }
+  ]
+};
