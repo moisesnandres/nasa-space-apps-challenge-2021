25ac309c0d2d10d453e6d333509d44ba29a2e7d5
Moisés Ñañez <moisesnandres@hey.com>
2021-10-03 19:01:58 -0500

    Add twitter button

--- components/DefrisCard.js
+++ components/DefrisCard.js
@@ -1,3 +1,5 @@
+import { TwitterShareButton, TwitterIcon } from "react-share";
+
 export default function DefrisCard({ closeModal }) {
   return (
     <div className="defris-card">
@@ -7,6 +9,13 @@ export default function DefrisCard({ closeModal }) {
         <h2>70% Marine plastic debris found</h2>
         <p className="question">Did you know lots of bottle microplastics particles eventually make their way into our waterways?</p>
         <p>Be the change you want to see in the planet. Share this information on Twitter <span className="hashtag">#tracktheplastic</span> and start taking action.</p>
+        <div className="twitter-button">
+          <TwitterShareButton title='I’ve found 70% Marine plastic debris on the 🌊 Atlantic Ocean 31.7771° -40.24965° with 20 meters depth at 
+⏰ 6:40 am. Did you know lots of bottle microplastics particles eventually make their way into our waterways? Share the information, let’s make a change #Tracktheplastic' url='https://nasa-space-apps-challenge-2021.vercel.app'>
+            <TwitterIcon size={32} round={true} />
+            Tweet
+          </TwitterShareButton>
+        </div>
       </div>
     </div>
   );
